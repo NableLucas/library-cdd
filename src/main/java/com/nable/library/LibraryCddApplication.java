06726bb8c89e20250e1b809c063eecb5ce940a8a
Nable Lucas <pnable.lucas@gmail.com>
2021-07-16 07:50:19 +0100

    first commit

---
+++ src/main/java/com/nable/library/LibraryCddApplication.java
@@ -0,0 +1,13 @@
+package com.nable.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryCddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryCddApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nable/library/LibraryCddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nable.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryCddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
