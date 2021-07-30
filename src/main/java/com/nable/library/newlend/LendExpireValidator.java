35f650789ab76e2c1c0d7df2c1b4b1ae57379e49
Nable Lucas <pnable.lucas@gmail.com>
2021-07-30 17:03:54 +0100

    feature/test-coverage

--- src/main/java/com/nable/library/newlend/LendExpireValidator.java
+++ src/main/java/com/nable/library/newlend/LendExpireValidator.java
@@ -5,12 +5,14 @@ import java.util.Objects;
 
 import javax.persistence.EntityManager;
 
+import org.springframework.stereotype.Component;
 import org.springframework.util.Assert;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
 import com.nable.library.newuser.User;
 
+@Component
 public class LendExpireValidator implements Validator {
 	
 	private EntityManager manager;
